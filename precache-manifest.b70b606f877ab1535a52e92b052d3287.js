a8bc0d67ceed396faa72d57ce4b933c16564c6ad
warmshadow <laukyskarolis@gmail.com>
2020-05-08 03:01:35 +0300

    Updates

---
+++ precache-manifest.b70b606f877ab1535a52e92b052d3287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4ccc73b81632d585aab361947fb46e",
+    "url": "/yahtzee-react/index.html"
+  },
+  {
+    "revision": "795779692d24aa88edf6",
+    "url": "/yahtzee-react/static/css/main.81dbfc1d.chunk.css"
+  },
+  {
+    "revision": "a80c6547f367f1620bf9",
+    "url": "/yahtzee-react/static/js/2.57ae64e0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/yahtzee-react/static/js/2.57ae64e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795779692d24aa88edf6",
+    "url": "/yahtzee-react/static/js/main.6d933202.chunk.js"
+  },
+  {
+    "revision": "9e3a5350b64a625bb008",
+    "url": "/yahtzee-react/static/js/runtime-main.90f26a71.js"
+  }
+]);
